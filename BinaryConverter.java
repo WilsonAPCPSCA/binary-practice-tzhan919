4531878a44f41a66d52781968acc17a2bbb416ac
tzhan919 <32201141+tzhan919@users.noreply.github.com>
2018-02-02 07:58:28 -0800

    Add files via upload
    w/ Hao Ming C

---
+++ BinaryConverter.java
@@ -0,0 +1,26 @@
+import java.util.Scanner ;
+public class BinaryConverter {
+	
+	public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int num=in.nextInt();
+System.out.println("Enter the number you would like to convert into binary: ");
+
+
+		
+		
+		
+		
+	}
+	public static double binary(double input){
+		double pos;
+		int x=999999999;
+		 for(double i=(int)Math.pow(2, x);i<=0;x--){
+			if(input<i){
+			pos=x;
+			}
+			return pos;
+		 }
+		 
+		}	
+}
